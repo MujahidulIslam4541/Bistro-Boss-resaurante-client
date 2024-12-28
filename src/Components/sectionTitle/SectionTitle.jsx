b21344339f9bea7085e5e108ceb9b4c35d3937e3
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-29 01:50:06 +0600

    create a dynamic section title

---
+++ src/Components/sectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SectionTitle = ({heading,subheading}) => {
+    return (
+        <div className='w-full md:w-1/3 lg:w-1/4 mx-auto text-center'>
+            <p className='text-[#D99904] font-semibold pb-2'>{subheading}</p>
+            <h2 className='text-3xl font-semibold border-y-4 py-4 border-gray-300'>{heading}</h2>
+
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/Pages/HomePage/Category/Category.jsx
+++ src/Pages/HomePage/Category/Category.jsx
@@ -12,10 +12,20 @@ import image2 from "../../../assets/home/slide2.jpg";
 import image3 from "../../../assets/home/slide3.jpg";
 import image4 from "../../../assets/home/slide4.jpg";
 import image5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../Components/sectionTitle/sectionTitle";
 
 const Category = () => {
   return (
+    <section className="my-20">
 
+    <SectionTitle  heading={"ORDER ONLINE"} subheading={"---From 11:00am to 10:00pm---"}>
+
+    </SectionTitle>
+
+
+
+
+        {/* Added swiper in category */}
       <Swiper
         slidesPerView={4}
         spaceBetween={10}
@@ -24,42 +34,58 @@ const Category = () => {
           clickable: true,
         }}
         modules={[FreeMode, Pagination]}
-        className="mySwiper mb-20"
+        className="mySwiper mb-20 mt-8"
       >
         <SwiperSlide>
           <img src={image1} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">SALADS</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">
+            SALADS
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image2} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">SOUPS</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">
+            SOUPS
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image3} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">PIZZA</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">
+            PIZZA
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image4} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center uppercase">desserts</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center uppercase">
+            desserts
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image5} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">SALADS</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">
+            SALADS
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image2} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">SOUPS</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">
+            SOUPS
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image3} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">PIZZA</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-16 text-center ">
+            PIZZA
+          </p>
         </SwiperSlide>
         <SwiperSlide>
           <img src={image4} alt="" />
-          <p className="text-3xl font-semibold text-white shadow-md -mt-12 text-center uppercase">desserts</p>
+          <p className="text-3xl font-semibold text-white shadow-md -mt-12 text-center uppercase">
+            desserts
+          </p>
         </SwiperSlide>
       </Swiper>
-
+    </section>
   );
 };
 
