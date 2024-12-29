4437b8a8a52cd093f84a0e7623812c707dd275e2
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-30 00:53:58 +0600

    all menu section display done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,19 +1,72 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../../Shaird/Cover/Cover";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../Components/sectionTitle/sectionTitle";
+import MenuCategory from "../menuCategory/MenuCategory";
 import CoverImage from "../../../assets/menu/banner3.jpg";
+import dessertImage from "../../../assets/menu/dessert-bg.jpeg";
+import saladImage from "../../../assets/menu/salad-bg.jpg";
+import pizzaImage from "../../../assets/menu/pizza-bg.jpg";
+import soupImage from "../../../assets/menu/pizza-bg.jpg";
 
 const Menu = () => {
+  const [menus] = useMenu();
+  const dessert = menus.filter((menu) => menu.category === "dessert");
+  const salad = menus.filter((menu) => menu.category === "salad");
+  const offered = menus.filter((menu) => menu.category === "offered");
+  const pizza = menus.filter((menu) => menu.category === "pizza");
+  const soup = menus.filter((menu) => menu.category === "soup");
   return (
     <div>
       <Helmet>
         <title>Bistro Boss / Menu</title>
       </Helmet>
 
+      {/* Pages cover */}
       <Cover
         img={CoverImage}
         heading="OUR MENU"
         description="Compellingly procrastinate enterprise-wide relationships rather than user-centric synergy. Assertively enable e-business infomediaries."
       ></Cover>
+
+      {/* offered menu section */}
+      <SectionTitle
+        heading="TODAY 'S OFFER"
+        subheading="---Don't miss---"
+      ></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+
+      {/* dessert menu section */}
+      <Cover
+        img={dessertImage}
+        heading="DESSERTS"
+        description="Compellingly procrastinate enterprise-wide relationships rather than user-centric synergy. Assertively enable e-business infomediaries."
+      ></Cover>
+      <MenuCategory items={dessert}></MenuCategory>
+
+      {/* Pizza menu section */}
+      <Cover
+        img={pizzaImage}
+        heading="PIZZA"
+        description="Compellingly procrastinate enterprise-wide relationships rather than user-centric synergy. Assertively enable e-business infomediaries."
+      ></Cover>
+      <MenuCategory items={pizza}></MenuCategory>
+
+      {/* Salad menu section */}
+      <Cover
+        img={saladImage}
+        heading="SALADS"
+        description="Compellingly procrastinate enterprise-wide relationships rather than user-centric synergy. Assertively enable e-business infomediaries."
+      ></Cover>
+      <MenuCategory items={salad}></MenuCategory>
+
+      {/* soup menu section */}
+      <Cover
+        img={soupImage}
+        heading="SOUPS"
+        description="Compellingly procrastinate enterprise-wide relationships rather than user-centric synergy. Assertively enable e-business infomediaries."
+      ></Cover>
+      <MenuCategory items={soup}></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/menuCategory/MenuCategory.jsx
+++ src/Pages/Menu/menuCategory/MenuCategory.jsx
@@ -1,11 +1,20 @@
+import MenuItem from "../../../Shaird/menuItem/MenuItem";
 
-
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items }) => {
+  return (
+    <div className="my-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 space-y-4 ">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="flex items-center justify-center mt-5">
+        <button className="border-0 border-b-2 rounded-md border-orange-500 text-orange-500 btn btn-outline">
+          ORDER YOUR FAVORITE FOOD
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
