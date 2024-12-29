33210d2f37599dede17c165356aba0305f3926eb
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-29 12:50:26 +0600

    added dynamic rating in testimonial section

--- src/Pages/Testimonial/Testimonial.jsx
+++ src/Pages/Testimonial/Testimonial.jsx
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionTitle from "../../Components/sectionTitle/sectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
 // import required modules
 import { Navigation } from "swiper/modules";
+// react awesome rating
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import { RiClockwise2Fill } from "react-icons/ri";
 
 const Testimonial = () => {
+  // Loaded reviews in json file
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("Review.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
   return (
     <section>
-
-        {/* section heading */}
+      {/* section heading */}
       <SectionTitle
         heading="TESTIMONIALS"
         subheading="---What Our Clients Say---"
       ></SectionTitle>
 
-{/* swiper slider */}
+      {/* swiper slider */}
       <div>
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className=" flex flex-col items-center my--16 mx-24">
+                <div className="flex gap-2 text-5xl my-4">
+                  <RiClockwise2Fill />
+                  <RiClockwise2Fill />
+                </div>
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review.rating}
+                  readOnly
+                />
+                <p>{review.details}</p>
+                <h2 className="text-orange-500 text-2xl uppercase font-semibold">
+                  {review.name}
+                </h2>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
